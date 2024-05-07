71cda371223b5ea767e0381fa4606690e3638201
Lelo <lelo.abera>
2024-05-07 18:03:27 +0300

    model test 1

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -53,26 +53,30 @@ export default function Page() {
     <>
       <div className="flex flex-col h-full p-2 md:p-2 pt-6 gap-2">
         <ScrollArea className="flex-grow h-5/6">
-          {messages?.map((message: any, index: number) => {
-            return (
-              <div
-                key={index}
-                className="flex justify-start gap-5 mb-5 items-start"
-              >
-                <Avatar>
-                  <AvatarImage
-                    src="https://github.com/shadcn.png"
-                    alt="@shadcn"
-                  />
-                  <AvatarFallback>WA</AvatarFallback>
-                </Avatar>
-                <div className="flex flex-col gap-2">
-                  <p>{message.role}</p>
-                  <div>{message.display}</div>
+          {messages?.length > 0 ? (
+            messages.map((message: any, index: number) => {
+              return (
+                <div
+                  key={index}
+                  className="flex justify-start gap-5 mb-5 items-start"
+                >
+                  <Avatar>
+                    <AvatarImage
+                      src="https://github.com/shadcn.png"
+                      alt="@shadcn"
+                    />
+                    <AvatarFallback>WA</AvatarFallback>
+                  </Avatar>
+                  <div className="flex flex-col gap-2">
+                    <p>{message.role}</p>
+                    <div>{message.display}</div>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })
+          ) : (
+            <div className="text-center text-purple-400 font-mono text-7xl"  >Welcome to Weather-AI!</div>
+          )}
         </ScrollArea>
         <div>
           <InputPrompt onSubmit={onSubmit} loading={loading} form={form} />
